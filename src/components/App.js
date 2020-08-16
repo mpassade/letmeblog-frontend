43eaabd688aae83318fa5208a745bf50247780bf
Michael Passade <michael.passade@codeimmersives.com>
2020-08-16 16:05:56 -0400

    finished bg css

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Hd
+
+class App extends Component {
+  componentDidMount(){
+    
+  }
+  render(){
+    return (
+      <>
+        <Header/>
+      </>
+    )
+  }
+}
+
+export default App
