cd331d0e982c863b3326e504d28f501ad7dc8cdb
Michael Passade <michael.passade@codeimmersives.com>
2020-08-23 00:26:38 -0400

    finished change pwd

--- src/components/ChangePwd.js
+++ src/components/ChangePwd.js
@@ -1,15 +1,57 @@
 import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
+import { Redirect, Link } from 'react-router-dom'
 import Nav from './Nav'
 import axios from 'axios'
 
 class ChangePwd extends Component {
     state = {
         user: {},
+        message: [{
+            type: '',
+            text: ''
+        }],
+        input: {
+            oldPass: '',
+            newPass: '',
+            confirmNew: ''
+        },
         isAuthenticated: true
     }
     isAuthenticated = () => {
         return localStorage.getItem('token')
+    }
+    handleChange = (e) => {
+        let input = {...this.state.input}
+        input[e.target.name] = e.target.value
+        this.setState({input})
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const axiosConfig = {
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
+        axios.put(`/change-password/${this.state.user.username}`, this.state.input, axiosConfig).then(res => {
+            let message = [...this.state.message]
+            message[0] = res.data
+            if (message[0].type==='success'){
+                let input = {
+                    oldPass: '',
+                    newPass: '',
+                    confirmNew: ''
+                }
+                this.setState({
+                    message,
+                    input
+                })
+            } else {
+                this.setState({message})
+            }
+        }).catch(err => {
+            console.log(`Server Error: ${err}`)
+        })
     }
     componentDidMount(){
         if (!this.isAuthenticated()){
@@ -38,7 +80,49 @@ class ChangePwd extends Component {
             <Nav
                 page='changePwd'
             />
-            
+            <form onSubmit={this.handleSubmit}>
+                <div
+                className={'change-pwd'}
+                >
+                    <header>Change Password</header>
+                    {this.state.message.map((msg, idx) => {
+                        return (
+                            <span
+                            key={idx}
+                            className={msg.type}
+                            >
+                                {msg.text}
+                            </span>
+                        )
+                    })}
+                    <input
+                        type='password'
+                        placeholder='Old Password'
+                        name='oldPass'
+                        onChange={this.handleChange}
+                        value={this.state.input.oldPass}
+                    />
+                    <input
+                        type='password'
+                        placeholder='New Password'
+                        name='newPass'
+                        onChange={this.handleChange}
+                        value={this.state.input.newPass}
+                    />
+                    <input
+                        type='password'
+                        placeholder='Confirm New Password'
+                        name='confirmNew'
+                        onChange={this.handleChange}
+                        value={this.state.input.confirmNew}
+                    />
+                    <div>
+                        <button type='submit' className='change'>Change</button>
+                        <Link to='/profile'><button className='cancel'>Cancel</button></Link>
+                    </div>
+                    
+                </div>
+            </form>
             </>
         )
     }
